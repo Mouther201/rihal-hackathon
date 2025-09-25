100edd8335792f248a528ce88fe0a14e38af3ef3
Mouther <malhashmi001@gmail.com>
2025-09-25 09:32:40 +0400

    testing auto

--- main.py
+++ main.py
@@ -1095,6 +1095,7 @@ if __name__ == "__main__":
     # Get port from environment variable or use default
     import os
     port = int(os.environ.get("PORT", 5000))
-    
+
+
     uvicorn.run(app, host="0.0.0.0", port=port)
 
